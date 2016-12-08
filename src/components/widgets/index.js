c285118eee515b9f2733a0311aa7fb8da1298a8b
Eric Lee <eric.lee@interface48.com>
2016-12-07 17:01:01 -0800

    Add EPBC widgets to widget registry

--- src/components/widgets/index.js
+++ src/components/widgets/index.js
@@ -6,13 +6,14 @@ import ColorWidget from "./ColorWidget";
 import DateWidget from "./DateWidget";
 import DateTimeWidget from "./DateTimeWidget";
 import EmailWidget from "./EmailWidget";
+import EPBCDateWidget from "./EPBCDateWidget";
 import FileWidget from "./FileWidget";
 import HiddenWidget from "./HiddenWidget";
 import PasswordWidget from "./PasswordWidget";
 import RadioWidget from "./RadioWidget";
 import RangeWidget from "./RangeWidget";
 import SelectWidget from "./SelectWidget";
-import TextareaWidget from "./TextareaWidget";
+import TextareaWidget from "./EPBCTextareaWidget";
 import TextWidget from "./TextWidget";
 import URLWidget from "./URLWidget";
 import UpDownWidget from "./UpDownWidget";
@@ -28,6 +29,7 @@ export default {
   DateWidget,
   DateTimeWidget,
   AltDateWidget,
+  EPBCDateWidget,
   AltDateTimeWidget,
   EmailWidget,
   URLWidget,
